6bf3b2d7288d70d645cc92af6de6ef1c4bf5f4cc
Wolfgang <wilguerrero1@gmail.com>
2013-07-15 15:21:33 -0400

    adding naive bayes classifier

---
+++ github/nbayes_tokenizer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import fileinput
+import json
+
+from sys import argv
+from base64 import b64encode
+
+USAGE = '''%s <list of filews>'''
+
+if __name__ == '__main__':
+
+	if len(argv) != 2:
+		print USAGE % argv[0]
+		exit(1)
+
+	bag_of_words = set()
+
+	for line in fileinput.input():
+		bag_of_words .update(set([b64encode(token) for token in line.split ()]))
+
+	print json.dumps(list(bag_of_words))
+ 
